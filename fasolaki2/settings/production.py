eaaa3a2866b6f2e93f67f74bd1f5be2d339e9aa7
chrys <zampas@gmail.com>
2025-05-03 23:28:23 +0100

    database in production.py

--- fasolaki2/settings/production.py
+++ fasolaki2/settings/production.py
@@ -2,6 +2,17 @@ from .base import *
 
 DEBUG = False
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+    }
+}
+
 #Security settings
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
